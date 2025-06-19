e3f491ec79ad19d51fc9b961fd3fe6f9cc9350c9
bartl <bartlomiej.stawiarski3@gmail.com>
2025-06-19 22:36:11 +0200

    first commit

---
+++ src/test/java/com/highhawk/ocr_system/OcrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highhawk.ocr_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
